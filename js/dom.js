75cfb51ddd124b2eba4ef58d617223a90123b793
AntonMyatto <48759290+AntonMyatto@users.noreply.github.com>
2020-07-22 12:26:15 +0300

    First array of words

--- js/dom.js
+++ js/dom.js
@@ -1,13 +1,36 @@
 const inputTranslate = document.getElementById('input');
-const inputBlock = document.getElementById('f1_block');
 const inputClear = document.getElementById('clear');
-
-inputTranslate.onclick = function(event){
-   inputBlock.style.borderColor = "red";
-   
-};
+const engWords = ["hello","today","i","we","he","she","it"];
+const rusWords = ["Привет","сегодня","Я","мы","он","она", "оно"];
+const btnTranslate = document.getElementById('button');
+const result = document.getElementById("result");
 
 inputClear.onclick = function(){
     inputTranslate.value = '';
+    result.innerHTML = "";
 }
+
+btnTranslate.onclick = () => {
+   if(inputTranslate.value == rusWords[0] || inputTranslate.value == 'привет'|| inputTranslate.value == 'Привет' ){
+       result.innerHTML = engWords[0];
+   }
+   if(inputTranslate.value == rusWords[1] || inputTranslate.value == 'Сегодня'|| inputTranslate.value == 'сегодня' ){
+    result.innerHTML = engWords[1];
+}
+if(inputTranslate.value == rusWords[2] || inputTranslate.value == 'Я'|| inputTranslate.value == 'я' ){
+    result.innerHTML = engWords[2];
+}
+if(inputTranslate.value == rusWords[3] || inputTranslate.value == 'МЫ'|| inputTranslate.value == 'мы' ){
+    result.innerHTML = engWords[3];
+}
+if(inputTranslate.value == rusWords[4] || inputTranslate.value == 'он'|| inputTranslate.value == 'Он' ){
+    result.innerHTML = engWords[4];
+}
+if(inputTranslate.value == rusWords[5] || inputTranslate.value == 'она'|| inputTranslate.value == 'Она' ){
+    result.innerHTML = engWords[5];
+}
+if(inputTranslate.value == rusWords[6] || inputTranslate.value == 'оно'|| inputTranslate.value == 'Оно' ){
+    result.innerHTML = engWords[6];
+}
+};
 
